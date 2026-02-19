fff23d178756e6d782a9359f8ba5207572e9fffc
edsonedwin123 <edsonedwin01@gmail.com>
2026-02-19 23:32:56 +0530

    s

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -28,7 +28,7 @@ const skills = [
   { name: "AIRTABLE", level: 0, category: "study" },
   { name: "N8N", level: 0, category: "study" },
   { name: "IOT", level: 3, category: "study" },
-  { name: "Git", level: 69, category: "study" },
+  { name: "Git", level: 89, category: "study" },
 ];
 
 const categories = ["all", "frontend", "backend", "tools","study"];
